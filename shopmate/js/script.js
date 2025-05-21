fdc8b19b9fea19606599ec8f3c3802cdb9957b66
Bopaki29 <bopakishaku@gmail.com>
2025-05-21 09:53:57 +0200

    Add files via upload

---
+++ shopmate/js/script.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const greetBtn = document.getElementById("greetBtn");
+    if (greetBtn) {
+        greetBtn.addEventListener("click", function () {
+            alert("Thanks for visiting ShopMate!");
+        });
+    }
+});
